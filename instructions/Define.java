469e92bc1bfbfa0e704a75d8f7f4791c440e7adf
DanielKlyus <77263885+DanielKlyus@users.noreply.github.com>
2021-06-05 18:43:38 +0700

    Update Define.java

--- instructions/Define.java
+++ instructions/Define.java
@@ -5,12 +5,16 @@ import com.company.CalculatorContext;
 public class Define implements Instruction {
     public void Execute(Object[] args, CalculatorContext ctx) throws InstructionException {
         //System.out.println(args[2]);
+
+        if ((args[1].toString()).matches("((-|\\\\+)?[0-9]+(\\\\.[0-9]+)?)+"))
+            throw new DefineException("bad 1st argument");
         try {
             ctx.getMap().put(args[1].toString(), Double.valueOf(args[2].toString()));
         }
         catch (NumberFormatException e) {
             throw new DefineException("bad 2nd argument");
         }
+
         //System.out.println(Double.valueOf(args[2].toString())*2);
     }
 }
